bf3d292594ef6a289202bbb89fd6108ebd0a5985
Allen Roulston <allenroulston@users.noreply.github.com>
2020-03-21 19:59:17 -0400

    new and improved pingpong login to discord

--- pingpong.rb
+++ pingpong.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 require 'discordrb'
 
-bot = Discordrb::Bot.new token: 'I00zRrXmu3LoFYRgIEe4t7qOC9tenXYJ', client_id: 690339632529015005
+bot = Discordrb::Bot.new token: I00zRrXmu3LoFYRgIEe4t7qOC9tenXYJ, client_id: 690339632529015005
 
 bot.message(with_text: 'Ping!') do |event|
   event.respond 'Pong!'
