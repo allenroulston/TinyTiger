0b3b9afcf4a17a1a0a26d79143fdfd0973a7553b
Allen Roulston <allenroulston@users.noreply.github.com>
2020-03-21 20:29:59 -0400

    slightly altered token and client setup

--- pingpong.rb
+++ pingpong.rb
@@ -1,7 +1,13 @@
-#!/usr/bin/ruby
+#####Configuration########
+token = 'NjkwMzM5NjMyNTI5MDE1MDA1.XnZ0uw.j4wXcl0SXHLG0Sx2EzWLK5lFUII' # Your bot's token
+prefix = "!" # Your bot's prefix
+owner = '690339632529015005' # Your user ID
+
+#####End Configuration####
+
 require 'discordrb'
 
-bot = Discordrb::Bot.new token: 'NjkwMzM5NjMyNTI5MDE1MDA1.XnZ0uw.j4wXcl0SXHLG0Sx2EzWLK5lFUII', client_id: '690339632529015005'
+bot = Discordrb::Bot.new token: token, client_id: owner
 
 bot.message(with_text: 'Ping!') do |event|
   event.respond 'Pong!'
