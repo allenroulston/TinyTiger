ed323bacd0944e5a9a0385fbda9c5b129d8f6918
Allen Roulston <allenroulston@users.noreply.github.com>
2020-03-21 20:04:27 -0400

    long token WITH quotes

--- pingpong.rb
+++ pingpong.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 require 'discordrb'
 
-bot = Discordrb::Bot.new token: NjkwMzM5NjMyNTI5MDE1MDA1.XnZ0uw.j4wXcl0SXHLG0Sx2EzWLK5lFUII, client_id: 690339632529015005
+bot = Discordrb::Bot.new token: 'NjkwMzM5NjMyNTI5MDE1MDA1.XnZ0uw.j4wXcl0SXHLG0Sx2EzWLK5lFUII', client_id: 690339632529015005
 
 bot.message(with_text: 'Ping!') do |event|
   event.respond 'Pong!'
