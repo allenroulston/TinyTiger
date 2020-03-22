299fcdc19ba24bed08d555e53dcf893fcd311872
Allen Roulston <allenroulston@users.noreply.github.com>
2020-03-22 14:32:45 -0400

    new and improved pingpong.rb file

--- pingpong.rb
+++ pingpong.rb
@@ -3,7 +3,15 @@ require 'discordrb'
 require 'yaml'
 
 #####Configuration########
-token = (YAML.load(File.read("data.yml"))).to_s;
+junk = YAML.load(File.read("data.yml"));
+puts;
+puts "--- here comes the junk value ";
+puts junk.inspect;
+puts;
+puts "here is the token";
+token = junk[0]+junk[1]+junk[2];
+puts token;
+puts;
 prefix = "!" # Your bot's prefix
 owner = 690339632529015005 # Your user ID
 
@@ -14,6 +22,17 @@ bot = Discordrb::Bot.new token: token
 bot.message(with_text: 'Ping!') do |event|
   event.respond 'Pong!'
 end
+
+bot.message(with_text: '[20]') do |event|
+  event.respond 'NATURAL 20'
+end
+
+bot.message(with_text: '[20]') do |event|
+  puts;
+  puts event.inspect;
+  puts;
+end
+
 
 bot.run
 
