3d968e6f9bbf38fd9f94be638417b36a82a8b923
Allen Roulston <allenroulston@users.noreply.github.com>
2020-03-21 20:37:36 -0400

    made the owner an integer value, not a character

--- pingpong.rb
+++ pingpong.rb
@@ -1,7 +1,7 @@
 #####Configuration########
 token = 'I00zRrXmu3LoFYRgIEe4t7qOC9tenXYJ' # Your bot's token
 prefix = "!" # Your bot's prefix
-owner = '690339632529015005' # Your user ID
+owner = 690339632529015005 # Your user ID
 
 #####End Configuration####
 
