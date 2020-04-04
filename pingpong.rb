311a003e3e410c7d52afce21ea9160b4007f85a1
Allen Roulston <allenroulston@users.noreply.github.com>
2020-04-03 22:23:43 -0400

    userNick

--- pingpong.rb
+++ pingpong.rb
@@ -49,9 +49,10 @@ bot.message(contains: "qqq") do |event|
 end;
 
 bot.message(contains: "d6") do |event|
-    userName = event.user.name
+    userName = event.user.name;
+    userNick = event.user.nick;
     tempVar = event.content;
-    event.respond userName.to_s; 
+    event.respond userName.to_s + userNick.to_s;
     #responseValue = "Someone rolled a d6 : " + ((rand 6)+1).to_s;
     event.respond responseValue;
 end;
