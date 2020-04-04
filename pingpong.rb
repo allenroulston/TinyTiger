53e35adf5de19525c4f0eac64565d501c599af94
Allen Roulston <allenroulston@users.noreply.github.com>
2020-04-03 21:36:00 -0400

    mod init
    "@everyone Please roll initiative as shown:  !roll d20 +dex  !init  <== !init (important) ";

--- pingpong.rb
+++ pingpong.rb
@@ -44,7 +44,7 @@ bot.message(contains: "rth") do |event|
 end;
 
 bot.message(contains: "ri") do |event|
-    responseValue = "@everyone Please roll initiative as shown:  !roll d20 + mod !init  <== !init (important) ";
+    responseValue = "@everyone Please roll initiative as shown:  !roll d20 +dex  !init  <== !init (important) ";
     event.respond responseValue;
 end;
 
