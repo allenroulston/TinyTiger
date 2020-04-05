f9fd217e1fa7c1c0df49221955370adb77e78223
Allen Roulston <allenroulston@users.noreply.github.com>
2020-04-05 16:21:40 -0400

    lines 328 - 335
    still trying to get a look at the content of the file

--- pingpong.rb
+++ pingpong.rb
@@ -326,10 +326,10 @@ bot.message(contains:"!!!") do |event|
              end;
              
              text=File.open('zero.txt').read
-             text.gsub!(/\r\n?/, "\n")
-             text.each_line do |line|
-                  acVal = line.inspect;
-             end;
+#             text.gsub!(/\r\n?/, "\n")
+#             text.each_line do |line|
+             acVal = text.slice(0,4);
+#             end;
 #             acVal = acVal.to_i;
              newACval = (12345).to_s+"\n";
              File.open("zero.txt", 'w+') {|f| f.write(newACval)}
