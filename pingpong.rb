2696f50371b13da07e689f58ecb4b4e50fcd09a7
Allen Roulston <allenroulston@users.noreply.github.com>
2020-04-17 11:52:25 -0400

    270-272
    Amended the output

--- pingpong.rb
+++ pingpong.rb
@@ -267,9 +267,9 @@ bot.message(start_with: ";rth") do |event|
       iRoll=(rand 20)+1; result = iRoll + mod + profB;
       say = @user.to_s + " rolled an attack: [" + iRoll.to_s + "] +" + mod.to_s + "+" + profB.to_s + " = " + result.to_s + "\n";
           if (result < @armour[target]) then;
-              say = say + "The attack Missed! creature " + target.to_s;
+              say = say + "The attack Missed!  -> creature " + target.to_s;
           else;
-              say = say + "The attack HIT! creature " + target.to_s;
+              say = say + "The attack HIT!  -> creature " + target.to_s;
               #check for iRoll to be 20 for a CRIT
               thePlayerIndex = @playerIndex;
               theWeaponIndex = @player[@playerIndex][1];
@@ -311,7 +311,8 @@ bot.message(start_with: ";rth") do |event|
           end;
     else;
        say = "Roll To Hit needs  ;rth?   ?= target number (0 to 9)";
-    end;    
+    end;
+    event.message.delete;
     event.respond say;
 end;
 
