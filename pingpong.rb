d80ec5d6ae1410604d3517f9a4cb64bfcb8ebc2d
Allen Roulston <allenroulston@users.noreply.github.com>
2020-04-19 12:53:42 -0400

    143-144
    Added more to the output and added newline

--- pingpong.rb
+++ pingpong.rb
@@ -140,8 +140,8 @@ bot.message(start_with: "myabs") do |event|;
     rangeDamage = @player[pIndex][12];
     say = theUser + " has Proficiency Bonus of " + prMod.to_s + "  and ability score modifiers of \n";
     say = say + "ST: " + stMod.to_s + "  DX: " + dxMod.to_s + "  CO: " + coMod.to_s + "  IN: " + inMod.to_s + "  WI: " + wiMod.to_s + "  CH: " + chMod.to_s + "\n";
-    say = say + "Default MELEE weapon damage is set to " + @weapon[meleeDamage];
-    say = say + "Default RANGED weapon damage is set to " + @weapon[rangeDamage];
+    say = say + "Default MELEE weapon damage is set to " + @weapon[meleeDamage] + "  (use $Wlist to see change options)";
+    say = say + "\nDefault RANGED weapon damage is set to " + @weapon[rangeDamage] + "  (use $Wlist to see change options)";
     event.respond say;
 end;
 
