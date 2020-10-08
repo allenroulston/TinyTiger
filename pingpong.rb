c21fa2d979a8434309e0c253c10962a17e304a74
Allen Roulston <allenroulston@users.noreply.github.com>
2020-10-07 22:36:43 -0400

    changes after line 1913

--- pingpong.rb
+++ pingpong.rb
@@ -1923,7 +1923,7 @@ bot.message(contains:"D500.") do |event|
       say = "Busted: " + chkNum.to_s;
     else;
          if chkNum > 0 then;
-            howManyDice = @tempVar.slice(0,(locationValue.to_i));
+            howManyDice = @tempVar.slice(0,(locationValue.to_i)-1);
             say = @user.to_s + " rolled " + howManyDice.to_s + "D500." + " + " + @whatPlus.to_s + "\n";
             die=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; total=0;
             (0..(howManyDice.to_i)).each do |x|;
