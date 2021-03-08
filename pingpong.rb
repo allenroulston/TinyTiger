68d40014a670271ed17ed63956ab5290ec816c9a
Allen Roulston <allenroulston@users.noreply.github.com>
2021-03-08 18:40:05 -0500

    lines 290 to 300
    corrected the index values to align with the correct abs mods

--- pingpong.rb
+++ pingpong.rb
@@ -288,16 +288,16 @@ bot.message(start_with: "$EDIT") do |event|;
       when "$EDITsh"; flag = true; stat=11;
    end;
    if flag == true then;
-     say = "st:" + player[pIndex][2].to_s + "  dx:" + player[pIndex][3].to_s + "  co:" + player[pIndex][4].to_s +
-          "  in:" + player[pIndex][5].to_s + "  wi:" + player[pIndex][6].to_s + "  ch:" + player[pIndex][7].to_s + "\n";
-     say = say + "ProfB:" + player[pIndex][7].to_s + "   MeleeW:" + player[pIndex][8].to_s + "   RangeW:" + player[pIndex][9].to_s + "\n";
-     say = say + "CastStat:" + player[pIndex][10].to_s;
+     say = "st:" + player[pIndex][1].to_s + "  dx:" + player[pIndex][2].to_s + "  co:" + player[pIndex][3].to_s +
+          "  in:" + player[pIndex][4].to_s + "  wi:" + player[pIndex][5].to_s + "  ch:" + player[pIndex][6].to_s + "\n";
+     say = say + "ProfB:" + player[pIndex][7].to_s +  "   CastStat:" + player[pIndex][10].to_s  + "\n";
+     say = say + "MeleeW:" + player[pIndex][8].to_s + "   RangeW:" + player[pIndex][9].to_s;
           
      player[pIndex][stat]=event.content.slice(7,1).to_i; #assignment of new stat modifier
 
      say = say + "\n The player stat mod for [" + event.content.slice(5,2) + "] is now: " + player[pIndex][stat].to_s;
-     say = say + "\n st:" + player[pIndex][2].to_s + "  dx:" + player[pIndex][3].to_s + "  co:" + player[pIndex][4].to_s +
-               "  in:" + player[pIndex][5].to_s + "  wi:" + player[pIndex][6].to_s + "  ch:" + player[pIndex][7].to_s;
+     say = say + "\n st:" + player[pIndex][1].to_s + "  dx:" + player[pIndex][2].to_s + "  co:" + player[pIndex][3].to_s +
+               "  in:" + player[pIndex][4].to_s + "  wi:" + player[pIndex][5].to_s + "  ch:" + player[pIndex][6].to_s;
                
 data = "---\n";
 #     we go through the list of characters and append the data to the preceeding data
